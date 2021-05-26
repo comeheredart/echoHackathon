f17a220a5b125a983d658914e7631f860a2b5e87
Jen <wonee0321@gmail.com>
2021-05-27 02:47:47 +0900

    [Feat] MapVC TableView 추가

--- echoHackathon/Sources/VC/MapViewController.swift
+++ echoHackathon/Sources/VC/MapViewController.swift
@@ -12,12 +12,22 @@ class MapViewController: UIViewController {
 
     //MARK: IBOutlet
     @IBOutlet var mapView: NMFMapView!
+    @IBOutlet var tableView: UITableView!
+    
+    //MARK: Variables
+    var sumValue: Int = 0
+    var Co2Value: Double = 0
+    var wonValue: Int = 0
     
     //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableViewSetting()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        print(sumValue)
     }
     
     //MARK: Function
@@ -26,8 +36,35 @@ class MapViewController: UIViewController {
         
     }
     
+    func tableViewSetting() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        
+        tableView.register(CompareTVC.Nib, forCellReuseIdentifier: CompareTVC.identifier)
+    }
+    
     
 
    
 
+}
+
+extension MapViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CompareTVC.identifier) as? CompareTVC else { return UITableViewCell() }
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
 }
