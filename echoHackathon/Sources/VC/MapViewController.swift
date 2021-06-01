172a2ec24f5e0c24f9d7a6b5aca853705aba4191
Jen <wonee0321@gmail.com>
2021-06-01 19:12:44 +0900

    [Fix] MapView UI Fix

--- echoHackathon/Sources/VC/MapViewController.swift
+++ echoHackathon/Sources/VC/MapViewController.swift
@@ -14,7 +14,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var mapView: NMFMapView!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var cityLabel: UILabel!
-    
+    @IBOutlet var segmentControl: UISegmentedControl!
     
     //MARK: Variables
     var locationManager: CLLocationManager!
@@ -24,14 +24,22 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     var Co2Value: Double = 0
     var wonValue: Int = 0
     
-    var titleArray = ["전력 사용량","절약된 전기세","탄소 배출량","나무 심기"]
+    var titleArray = ["전력 사용량","절약된 전기세","탄소 배출량"]
+    var unitArray = ["kwh", "원", "kg"]
     var valueArray : [Int] = []
     var cityDic : [String: String] = ["Seoul":"서울특별시", "Gwangju":"광주광역시", "Incheon":"인천광역시", "Daegu":"대구광역시","Ulsan":"울산광역시","Daejeon":"대전광역시","Busan":"부산광역시","Gyeonggi-do":"경기도", "Gangwon-do":"강원도", "Chungcheongnam-do":"충청남도","Chung-cheong bukdo":"충청북도","Gyeongsangbuk-do":"경상북도","Gyeongsangnam-do":"경상남도","Jeollabuk-do":"전라북도", "Jeollanam-do":"전라남도", "Jeju-do":"제주도"]
     
+    var cityAvg = ["Seoul":212, "Incheon": 198] //22439 //21449
+    var goodEx = [170, 0000, 0000] //전기세랑 탄소 배출량 계산 ..
+    var badEx = [250, 0000, 0000]
+    var topGoodValArr = [5, 7, 4]
+    var topBadValArr = [84, 75, 91]
     
     //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        segmentControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
         
         tableViewSetting()
         locationSetting()
@@ -40,7 +48,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         
         valueArray = [self.sumValue, self.wonValue, Int(self.Co2Value), 0]
     }
-    
     
     //MARK: Function
     
@@ -129,6 +136,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         })
     }
     
+    @objc func segmentChanged() {
+        tableView.reloadData()
+    }
+    
+    
     //MARK: @IBAction
     @IBAction func otherCityBtnClicked(_ sender: UIButton) {
         //AlertView 띄우기
@@ -140,7 +152,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
 extension MapViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -148,7 +160,30 @@ extension MapViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CompareTVC.identifier) as? CompareTVC else { return UITableViewCell() }
         
         cell.titleLabel.text = titleArray[indexPath.row]
-        cell.useLabel.text = "\(valueArray[indexPath.row])"
+        //cell.useLabel.text = "\(valueArray[indexPath.row])" //이걸 good 이나 bad 로 바꾸기
+        
+        let idx = indexPath.row
+        
+        switch segmentControl.selectedSegmentIndex {
+        
+        case 0:
+            cell.topView.backgroundColor = .green
+            cell.morelessLabel.text = "적게 소비"
+            cell.useLabel.text = "\(goodEx[idx]) \(unitArray[idx])"
+            cell.topValueLabel.text = "\(topGoodValArr[idx]) %"
+            break
+            
+        case 1:
+            cell.topView.backgroundColor = .red
+            cell.morelessLabel.text = "많이 소비"
+            cell.useLabel.text = "\(badEx[indexPath.row]) \(unitArray[idx])"
+            cell.topValueLabel.text = "\(topBadValArr[indexPath.row]) %"
+            break
+            
+        default:
+            break
+        }
+        
         
         return cell
         
@@ -160,25 +195,4 @@ extension MapViewController: UITableViewDelegate, UITableViewDataSource {
     
     
 }
-
-//extension MapViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return cityArray.count
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return cityArray[row]
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-//        return 60
-//    }
-//
-//
-//}
-
 
